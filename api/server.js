17a22d3be6de13f77334a3bb17a1d4444fad7ba4
Holly919 <56270260+Holly919@users.noreply.github.com>
2020-10-09 21:12:15 -0400

    initial commit

--- api/server.js
+++ api/server.js
@@ -15,4 +15,9 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+// server.get("/", (req, res) => {
+//     res.json({ api: "I changed all my passwords to 'Incorrect'. So whenever I forget, it will tell me 'Your password is Incorrect'." });
+//   });
+  
+
 module.exports = server;
